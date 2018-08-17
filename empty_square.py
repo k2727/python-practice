ea4de8144ca0be1fc71fdaee805d541f063c99c1
k2727 <42423956+k2727@users.noreply.github.com>
2018-08-17 15:33:03 -0600

    Update empty_square.py

--- empty_square.py
+++ empty_square.py
@@ -1,11 +1,11 @@
 def square(n):
-    max_spaces = n-2
+    spaces = n -2
     for i in range(1, n + 1):
         if i == 1:
             print('*' * n)
         elif i<n:
-            print("*"+' ' * max_spaces + "*")
-        else: print('*' * n)
+            print("*" + ' ' * spaces + "*")
+        else: print('*'  *  n)
 
 
 required_number = int(input("Enter Number:"))
