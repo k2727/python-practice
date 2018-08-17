7d4e322279041dfdec2b6bf11e7e6b0843630c6b
k2727 <42423956+k2727@users.noreply.github.com>
2018-08-16 23:07:14 -0600

    Update rombo.py

--- rombo.py
+++ rombo.py
@@ -4,12 +4,11 @@ def diamond(n):
     for blanks in range(max_spaces, -1, -1):
         print(blanks * ' ' + stars * '*')
         stars = stars + 2
-
         star = stars -4
+        
     for blanks in range(1, max_spaces + 1):
         print(blanks * ' ' + star * '*')
         star = star - 2
-
 
 
 required_number = int(input('Enter Number'))
